8cc0b322cffb7ee78a1ebc40ecd0bbbd7e9a5642
Joshua Luce <joshuabluce@protonmail.com>
2021-07-16 12:41:49 -0500

    Add some debug text

--- config/airootfs/root/.kickstart/post-install-scripts/06-bootloader.sh
+++ config/airootfs/root/.kickstart/post-install-scripts/06-bootloader.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 bootloader_efi() {
+  echo "Installing systemd-boot..."
   bootctl install
   sed -i '/^HOOKS=/ s/)/ systemd)/' /etc/mkinitcpio.conf
   mkinitcpio -P
 }
 
 bootloader_mbr() {
+  echo "Installing GRUB..."
   pacman -S grub
   grub-intall --target=i386-pc /dev/sda1
   grub-mkconfig -o /boot/grub/grub.cfg
