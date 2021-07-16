b00a8b961f03ecccbc06e20cae8e1e1657f971e7
Joshua Luce <joshuabluce@protonmail.com>
2021-07-16 14:19:01 -0500

    Fix nobody user not being usable

--- config/airootfs/root/.kickstart/post-install-scripts/03-install-paru.sh
+++ config/airootfs/root/.kickstart/post-install-scripts/03-install-paru.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 git clone https://aur.archlinux.org/paru.git /tmp/paru
+nobodyshell=$(cat /etc/passwd | grep nobody | cut -d ':' -f 7)
+chsh nobody -s /bin/bash
 cd /tmp/paru
 su nobody -c "makepkg -si"
+chsh nobody -s $nobodyshell
