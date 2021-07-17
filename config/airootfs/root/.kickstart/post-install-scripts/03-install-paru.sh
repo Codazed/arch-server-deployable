3f62580392d6fd0705af522999a0036d156882a6
Joshua Luce <joshuabluce@protonmail.com>
2021-07-16 23:18:54 -0500

    Fix permissions issue for Paru

--- config/airootfs/root/.kickstart/post-install-scripts/03-install-paru.sh
+++ config/airootfs/root/.kickstart/post-install-scripts/03-install-paru.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 pacman -S git --noconfirm
 git clone https://aur.archlinux.org/paru.git /tmp/paru
+chown -R nobody:nobody /tmp/paru
 nobodyshell=$(cat /etc/passwd | grep nobody | cut -d ':' -f 7)
 chsh nobody -s /bin/bash
 cd /tmp/paru
