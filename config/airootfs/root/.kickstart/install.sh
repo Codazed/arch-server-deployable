cd64008815ce4b432ca47419e35db7f358947cdc
Joshua Luce <joshuabluce@protonmail.com>
2021-07-16 23:22:14 -0500

    Implement calling of post-chroot scripts

--- config/airootfs/root/.kickstart/install.sh
+++ config/airootfs/root/.kickstart/install.sh
@@ -4,7 +4,7 @@ source ./lib.sh
 log_info "Starting automated install"
 sleep 5
 log_info "Running pre-install scripts"
-for script in $(find pre-install-scripts -type f)
+for script in $(find pre-install-scripts -iname *.sh -type f)
 do
   log_info "-$(basename $script)"
   source $script
@@ -22,13 +22,22 @@ log_info "Generating fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
 
 log_info "Running post-install scripts"
-for script in $(find post-install-scripts -type f)
+for script in $(find post-install-scripts -iname *.sh -type f)
 do
   log_info "-$(basename $script)"
   cat $script | arch-chroot /mnt bash
   log_pass "+$(basename $script)"
 done
 log_pass "Finished running post-install scripts"
+sleep 3
+log_info "Running post-chroot scripts"
+for script in $(find post-chroot-scripts -iname *.sh -type f)
+do
+  log_info "-$(basename $script)"
+  source $script
+  log_pass "+$(basename $script)"
+done
+log_pass "Finished running post-chroot scripts"
 sleep 3
 log_info "Unmounting file systems"
 umount -R /mnt
--- config/airootfs/root/.kickstart/post-chroot-scripts/00-copy-bootloader-config.sh
+++ config/airootfs/root/.kickstart/post-chroot-scripts/00-copy-bootloader-config.sh
@@ -1,9 +1,3 @@
 if [[ $(ls /sys/firmware/efi/efivars 2>/dev/null) ]]; then
-  efisys=true
-else
-  efisys=false
-fi
-
-if [[ $efisys ]]; then
   cp systemd-bootentry.conf /mnt/boot/loader/entries/
 fi
